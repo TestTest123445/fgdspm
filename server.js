7084aea8293ecd6c6b379e52cb133f74e5bcc8da
TestTest123445 <166025121+TestTest123445@users.noreply.github.com>
2024-04-04 18:30:45 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+// Using require statements to include necessary modules
+const http = require('http');
+const app = require('./app'); // Make sure this module is correctly pointing to your app's entry point
+const port = process.env.PORT || 3000;
+
+const { Telegraf } = require('telegraf');
+
+// Using an environment variable for the token
+const TOKEN = process.env.BOT_TOKEN;
+
+const bot = new Telegraf(TOKEN);
+
+// Creating the server
+const server = http.createServer(app);
+
+// Server listens on the specified port
+server.listen(port, () => {
+    console.log(`Started on port ${port}`); // Corrected to use template literals
+});
+
+// Web link for the web app
+const web_link = "https://dreamy-haupia-0f980c.netlify.app/";
+
+// Setting up the bot's start command
+bot.start((ctx) => 
+    ctx.reply('Welcome 1234', {
+        reply_markup: {
+            keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+        },
+    })
+);
+
+// Launching the bot
+bot.launch().catch((err) => {
+    console.error('Error launching bot:', err);
+});
